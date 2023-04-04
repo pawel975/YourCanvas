857f646715e2ad79bf9d2ce1eb2f84c095d3dca8
Pawel  Kurek <pawel.kurek975@gmail.com>
2023-04-04 22:19:18 +0200

    ToolBar initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 import Main from './pages/Main/Main'
 import NavBar from './layouts/NavBar/NavBar'
+import ToolBar from './layouts/ToolBar/ToolBar'
 
 const App = () => {
 
   return (
     <div className="app">
       <NavBar/>
-      <Main/>
+      <div className='app__creator-wrapper'>
+        <ToolBar/>
+        <Main/>
+      </div>
     </div>
   )
 }
---
+++ src/layouts/ToolBar/ToolBar.tsx
@@ -0,0 +1,11 @@
+import './ToolBar.css';
+
+const ToolBar: React.FC = () => {
+    return (
+        <div className="tool-bar">
+            
+        </div>
+    )
+}
+
+export default ToolBar;
