6c4673781914bb28b01937a2fc09411e8aba7e32
Pawel  Kurek <pawel.kurek975@gmail.com>
2023-04-18 21:18:59 +0200

    tool bar shows on project init

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import Main from './pages/Main/Main';
 import NavBar from './layouts/NavBar/NavBar';
 import ToolBar from './layouts/ToolBar/ToolBar';
 import React from 'react';
+import { useAppSelector } from './redux/hooks';
 
 const App: React.FC = () => {
+  const projectType = useAppSelector((state) => state.createNewProjectButton.projectType);
+
   return (
     <div className="app">
       <NavBar />
       <div className="app__creator-wrapper">
-        <ToolBar />
+        {projectType !== 'blank' && <ToolBar />}
         <Main />
       </div>
     </div>
