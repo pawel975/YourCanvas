8baca3ffe60fbcaffb59ecb1e3c1e72e27f3b40d
Pawel  Kurek <pawel.kurek975@gmail.com>
2023-04-20 22:37:29 +0200

    drawing container refactor

--- src/data/errors.ts
+++ src/data/errors.ts
@@ -1,6 +1,7 @@
 const ERRORS = {
   INVALID_CONTEXT: 'InvalidCanvasContextError',
   INVALID_SNAPSHOT: 'InvalidSnapshot',
+  INVALID_CANVAS_REF: 'InvalidCanvasRef',
 };
 
 export default ERRORS;
--- src/pages/Main/DrawingContainer/DrawingContainer.tsx
+++ src/pages/Main/DrawingContainer/DrawingContainer.tsx
@@ -6,6 +6,16 @@ import getRectDrawHandlers from '../../../features/drawing/rectDraw/getRectDrawH
 import { Eventhandlers } from '../../../globalInterfaces';
 import { useAppSelector } from '../../../redux/hooks';
 import { getSprayDrawHandlers } from '../../../features/drawing/sprayDraw';
+import ERRORS from '../../../data/errors';
+
+interface CanvasSize {
+  width: string;
+  height: string;
+}
+
+type DrawHandlers = {
+  [key: string]: Eventhandlers;
+};
 
 const DrawingContainer: React.FC = () => {
   const [mouseListeners, setMouseListeners] = useState<Eventhandlers>({
@@ -15,11 +25,13 @@ const DrawingContainer: React.FC = () => {
   });
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const canvasContainerRef = useRef<HTMLDivElement>(null);
-  const [canvasSize, setCanvasSize] = useState({ width: '', height: '' });
+  const [canvasSize, setCanvasSize] = useState<CanvasSize>({ width: '', height: '' });
 
-  const currentToolId = useAppSelector((state) => state.toolSelection.tool);
-  const currentColorHex = useAppSelector((state) => state.colorSelection.color);
-  const currentToolSize = useAppSelector((state) => state.toolSizeSelection.size);
+  const currentToolId: string | number = useAppSelector((state) => state.toolSelection.tool);
+  const currentColorHex: string = useAppSelector((state) => state.colorSelection.color);
+  const currentToolSize: number = useAppSelector((state) => state.toolSizeSelection.size);
+
+  const [isError, setIsError] = useState<boolean>(false);
 
   useEffect(() => {
     setCanvasSize({
@@ -29,42 +41,48 @@ const DrawingContainer: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    if (canvasRef.current) {
-      switch (currentToolId) {
-        case 'marker':
-          setMouseListeners(
-            getMarkerDrawHandlers(currentColorHex, currentToolSize, canvasRef.current)
-          );
-          break;
-        case 'rect':
-          setMouseListeners(
-            getRectDrawHandlers(currentColorHex, currentToolSize, canvasRef.current)
-          );
-          break;
-        case 'spray':
-          setMouseListeners(
-            getSprayDrawHandlers(currentColorHex, currentToolSize, canvasRef.current)
-          );
-          break;
+    try {
+      if (!canvasRef.current) {
+        const error = new Error(`Invalid canvas ref - ${canvasRef.current}`);
+        error.name = ERRORS.INVALID_CANVAS_REF;
+        throw error;
       }
+
+      const drawHandlers: DrawHandlers = {
+        marker: getMarkerDrawHandlers(currentColorHex, currentToolSize, canvasRef.current),
+        rect: getRectDrawHandlers(currentColorHex, currentToolSize, canvasRef.current),
+        spray: getSprayDrawHandlers(currentColorHex, currentToolSize, canvasRef.current),
+      };
+
+      setMouseListeners(drawHandlers[currentToolId]);
+    } catch (error) {
+      console.error(error);
+      setIsError(true);
     }
   }, [currentToolId, currentColorHex, currentToolSize]);
 
   return (
-    <div
-      ref={canvasContainerRef}
-      className="project-container"
-    >
-      <canvas
-        className="project-canvas"
-        width={canvasSize.width}
-        height={canvasSize.height}
-        ref={canvasRef}
-        onMouseDown={mouseListeners.mouseDownHandler}
-        onMouseMove={mouseListeners.mouseMoveHandler}
-        onMouseUp={mouseListeners.mouseUpHandler}
-      ></canvas>
-    </div>
+    <>
+      {isError ? (
+        // Create Error message component template instead h1
+        <p>Error</p>
+      ) : (
+        <div
+          ref={canvasContainerRef}
+          className="project-container"
+        >
+          <canvas
+            className="project-canvas"
+            width={canvasSize.width}
+            height={canvasSize.height}
+            ref={canvasRef}
+            onMouseDown={mouseListeners.mouseDownHandler}
+            onMouseMove={mouseListeners.mouseMoveHandler}
+            onMouseUp={mouseListeners.mouseUpHandler}
+          ></canvas>
+        </div>
+      )}
+    </>
   );
 };
 
