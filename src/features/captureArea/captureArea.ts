d8178f48853c4f081a3ba209d864264e4c654270
Pawel  Kurek <pawel.kurek975@gmail.com>
2023-04-28 17:28:58 +0200

    added constants to captureArea function instead multiple hardcoded values

--- src/features/captureArea/captureArea.ts
+++ src/features/captureArea/captureArea.ts
@@ -5,15 +5,20 @@ export function captureArea(
   end: CanvasCoordinates,
   ctx: CanvasRenderingContext2D
 ) {
+  const WIDTH = 1;
+  const COLOR = '#000000';
+  const DASH_SIZE = [3];
+
   start = start ?? end;
   ctx.beginPath();
-  ctx.lineWidth = 3;
-  ctx.strokeStyle = '#000000';
+  ctx.lineWidth = WIDTH;
+  ctx.strokeStyle = COLOR;
   ctx.strokeRect(start.x, start.y, end.x - start.x, end.y - start.y);
   ctx.stroke();
+  ctx.setLineDash(DASH_SIZE);
 
-  ctx.fillStyle = '#000000';
+  ctx.fillStyle = COLOR;
   ctx.beginPath();
-  ctx.arc(start.x, start.y, 3, 0, 2 * Math.PI);
+  ctx.arc(start.x, start.y, WIDTH, 0, 2 * Math.PI);
   ctx.fill();
 }
